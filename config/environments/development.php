8266bf2219a6a3226e101e6dfed9133bf4ac7f9b
Chris Gilligan <chris-gilligan@utc.edu>
2021-12-03 09:11:48 -0500

    UTCB: Disable database repair in development environment.
    Signed-off-by: Chris Gilligan <chris-gilligan@utc.edu>

--- config/environments/development.php
+++ config/environments/development.php
@@ -13,9 +13,11 @@ Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
-Config::define('WP_ALLOW_REPAIR', true);
 
 ini_set('display_errors', '1');
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
+
+// Enable database repair via wp-admin/maint/repair.php
+//Config::define('WP_ALLOW_REPAIR', true);
